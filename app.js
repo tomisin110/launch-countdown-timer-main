1cebbd232c15afd58152fe21c914e0674fbe80df
tomisin110 <54535614+tomisin110@users.noreply.github.com>
2022-05-31 10:57:48 +0100

    committed

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-  const endDate = new Date("December 25, 2021 00:00:00").getTime()
+  const endDate = new Date("December 25, 2022 00:00:00").getTime()
   const now = new Date().getTime()
 
   const difference = endDate - now
